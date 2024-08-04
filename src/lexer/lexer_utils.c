0e43dc37ec94290f42549f61ee1c7061446877f2
Mahmoud Habbal <mhabbal@c1r4s2.42beirut.com>
2024-08-04 09:55:32 +0300

    working on the bonus

---
+++ src/lexer/lexer_utils.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhabbal <mhabbal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/04 09:02:56 by mhabbal           #+#    #+#             */
+/*   Updated: 2024/08/04 09:52:42 by mhabbal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	handle_and_state(t_lexer *lex, t_token **token, int type, int len)
+{
+	if (type == TYPE_AMPERSAND)
+	{
+		(*token)->value[lex->util->j++] = type;
+		(*token)->value[lex->util->j] = '\0';
+		(*token)->next = ft_calloc(1, sizeof(t_token));
+		if ((*token)->next == NULL)
+			return ;
+		*token = (*token)->next;
+		init_token((*token), len - lex->util->i);
+		lex->util->j = 0;
+	}
+	else if (type != TYPE_AMPERSAND)
+	{
+		(*token)->value[lex->util->j] = '\0';
+		(*token)->next = ft_calloc(1, sizeof(t_token));
+		if ((*token)->next == NULL)
+			return ;
+		*token = (*token)->next;
+		init_token((*token), len - lex->util->i);
+		lex->util->j = 0;
+		if (type != TYPE_SPACE)
+			(*token)->value[lex->util->j++] = lex->util->c;
+	}
+}
+
+void	lex_and(t_lexer *lex, t_token **token, int *state, int type, int len)
+{
+	if (lex->util->j > 0)
+	{
+		(*token)->value[lex->util->j] = '\0';
+		(*token)->next = ft_calloc(1, sizeof(t_token));
+		if ((*token)->next == NULL)
+			return ;
+		*token = (*token)->next;
+		init_token((*token), len - lex->util->i);
+		lex->util->j = 0;
+	}
+	(*token)->value[lex->util->j++] = type;
+	(*token)->type = type;
+	(*state) = IN_AND;
+}
+
+void	handle_or_state(t_lexer *lex, t_token **token, int type, int len)
+{
+	if (type == TYPE_PIPE)
+	{
+		(*token)->value[lex->util->j++] = type;
+		(*token)->value[lex->util->j] = '\0';
+		(*token)->next = ft_calloc(1, sizeof(t_token));
+		if ((*token)->next == NULL)
+			return ;
+		*token = (*token)->next;
+		init_token((*token), len - lex->util->i);
+		lex->util->j = 0;
+	}
+	else if (type != TYPE_PIPE)
+	{
+		(*token)->value[lex->util->j] = '\0';
+		(*token)->next = ft_calloc(1, sizeof(t_token));
+		if ((*token)->next == NULL)
+			return ;
+		*token = (*token)->next;
+		init_token((*token), len - lex->util->i);
+		lex->util->j = 0;
+		if (type != TYPE_SPACE)
+			(*token)->value[lex->util->j++] = lex->util->c;
+	}
+}
+
+void	lex_or(t_lexer *lex, t_token **token, int *state, int type, int len)
+{
+	if (lex->util->j > 0)
+	{
+		(*token)->value[lex->util->j] = '\0';
+		(*token)->next = ft_calloc(1, sizeof(t_token));
+		if ((*token)->next == NULL)
+			return ;
+		*token = (*token)->next;
+		init_token((*token), len - lex->util->i);
+		lex->util->j = 0;
+	}
+	(*token)->value[lex->util->j++] = type;
+	(*token)->type = type;
+	(*state) = IN_OR;
+}
