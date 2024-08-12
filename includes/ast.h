02639f08f08080d21256b85ae4678cff6406be86
Nayef Kanaan <nbk01.dev@gmail.com>
2024-08-12 20:31:54 +0300

    planning ast

--- includes/ast.h
+++ includes/ast.h
@@ -2,6 +2,55 @@
 # define AST_H
 # include "./minishell.h"
 
+struct s_synatx_tree;
+
+// Follow BNF Format
+
+typedef enum node_type
+{
+	AST_PIPE,
+	AST_REDIRECT,
+}	e_node_type;
+
+typedef struct s_ast_node
+{
+	char	**args;			// 2d array for exec?? maybe single ptr -- check again
+	struct s_ast_node	*left;  // Normal binary tree stuff
+	struct s_ast_node	*right; // Normal binary tree stuff
+	e_node_type	type; // Type of node -> if pipe takes left and right 
+						// IF Simplecmd takes in command char 
+						// and init 2d array for exec
+	int	priority // ?????? to check for parens??
+}	t_ast_node;
+
+typedef struct s_syntax_tree
+{
+	struct s_ast_node	*branch;
+	struct s_syntax_tree	*next;
+}	t_syntax_tree;
+
+void		init_parser(t_lexer **lexer);
+void		p_build_tree(t_token *token, t_ast_node *ast);
+
+/* 
+ * Create function for all cases :::
+ * -> REDIRECT IN
+ *  -> REDIRECT OUT
+ *  -> APPEND MODE
+ *  -> PIPE
+ *  -> SIMPLE COMMAND
+ *  -> HEREDOC
+ *  -> AND - OR - PIPE ---- IN THIS ORDER
+ *  AFER BUILDING SINGLE NODES APPEND INTO MAIN STRUCT TO EXECUTE BASED ON PRIORITY ??
+ */
+t_ast_node	*p_build_simple_command(char *cmd);
+t_ast_node	*p_build_pipe(t_ast_node *left, t_ast_node *right);
+t_ast_node	*p_build_redirect(t_ast_node *left, char *infile);
+
+// ITERATE THROUGH TOKEN LIST IF NO DELIMITER APPEND TO SIMPLE COMMAND
+
+//  !!!!!!!!!!!!!!!!!!!DONT SCROLL FURTHER!!!!!!!!!!!!!!!!!!!!!!
+
 //////////////////// AST TEMPLATE ///////////////////////
 /*
                  __ PIPE__
@@ -43,16 +92,6 @@
  *
 **/
 
-struct s_tree;
-
-typedef enum ast_type
-{
-	AST_REDIRECT_IN,
-	AST_REDIRECT_OUT,
-	AST_HEREDOC,
-	AST_APPEND,
-}	e_ast_type;
-
 /* typedef union	ast_unions
 {
 	struct AST_SIMPLECMD
@@ -79,20 +118,30 @@ typedef enum ast_type
 }	u_ast_unions;
 */
 
+// DO MORE RESEARCH ON HOW UNIONS WORK IN C AND HOW WE CAN UTILIZE IT WHILE PARSING
+
+
+/* BNF GRAMMAR FOR THE SHELL
+<letter> ::= a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z| 
+	A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z 
+<digit> ::= 0|1|2|3|4|5|6|7|8|9 <number> ::= <digit> | <number> <digit> 
+<word> ::= <letter> | <word> <letter> | <word> '_' 
+<word_list> ::= <word> | <word_list> <word> 
+<assignment_word> ::= <word> '=' <word> 
+<redirection> ::= '>' <word> | '<' <word> | <number> '>' <word> | <number> '<' <word> | '>>' <word> | <number> '>>' <word> | '<<' <word> | <number> '<<' <word> 
+<simple_command_element> ::= <word> | <assignment_word> | <redirection> 
+<redirection_list> ::= <redirection> | <redirection_list> <redirection> 
+<simple_command> ::= <simple_command_element> | <simple_command> <simple_command_element> 
+<command> ::= <simple_command> | <shell_command> | <shell_command> <redirection_list> 
+*/
+
+/*
 typedef struct s_leaf
 {
 	char	**value;
 	e_ast_type type;
 }	t_leaf;
+*/
 
-typedef struct s_tree
-{
-	t_leaf	*root;
-	t_leaf	*left;
-	t_leaf	*right;
-}	t_tree;
-
-void	init_parser(t_lexer **lexer);
-void	p_build_tree(t_token *token, t_tree *ast);
 
 #endif
--- src/parser/p_build.c
+++ src/parser/p_build.c
@@ -1,9 +1,17 @@
 #include "../../includes/ast.h"
 #include "../../includes/token.h"
 
-void	p_build_tree(t_token *token, t_tree *ast)
+void	p_build_tree(t_token *token, t_ast_node *ast)
 {
-	(void)ast;
+	if (token->type == TOKEN)
+		// SEND TO BUILD COMMAND
+		// BUILD 2D ARRAY OF CMD AND ARGS -> INIT NODE OUT OF THIS
 	if (token->type == TYPE_PIPE)
-		printf("THIS IS A PIPE\n");
+		// SEND TO BUILD PIPE
+		// TAKES IN LEFT NODE RETURNED FROM BUILD COMMAND AND RIGHT CMD RETURNED FROM BUILD CMD
+	if (token->type == TYPE_LSHIFT)
+		// SEND TO REDIRECT
+		// SAME SAME
+	
+	// ETC ETC
 }
