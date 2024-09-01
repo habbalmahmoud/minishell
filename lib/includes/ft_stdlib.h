2a956c3df480f9644e25808321f562246fe777a7
excelsior <nayefkanaan03@gmail.com>
2024-09-01 13:33:16 +0300

    hh

--- lib/includes/ft_stdlib.h
+++
@@ -1,9 +0,0 @@
-#ifndef FT_STDLIB_H
-# define FT_STDLIB_H
-# include "./libft.h"
-
-
-int		ft_atoi(const char *str);
-void	*ft_calloc(size_t count, size_t size);
-
-#endif
--- lib/srcs/ctype/ft_isspace.c
+++
@@ -1,9 +0,0 @@
-
-#include "../../includes/ft_ctype.h"
-
-int	ft_isspace(int c)
-{
-	if (c == 32 || (c >= 9 && c <= 13)) 
-		return (0);
-	return (1);
-}
